a134df79b4cfe3d6cad5992b02eabfe214d336f6
Braulio_Bolano <brauliobolanostrujillo@gmail.com>
2024-03-02 06:19:11 -0500

    Finished timeline for portfolio

--- src/components/Timeline/TimelineObject.jsx
+++ src/components/Timeline/TimelineObject.jsx
@@ -1,4 +1,4 @@
-const TimelineObject = ({ cardDate, cardTitle, cardTitle2, cardText1, cardText2, cardText3, cardText4, cardText5, cardText6, cardText7, titleSVG, centerSVG}) => {
+const TimelineObject = ({ cardDate, cardTitle, cardTitle2, cardText1, cardText2, cardText3, cardText4, cardText5, cardText6, cardText7, titleSVG, centerSVG, titleLink}) => {
     
 
     return (
@@ -10,17 +10,15 @@ const TimelineObject = ({ cardDate, cardTitle, cardTitle2, cardText1, cardText2,
                         <div className="flex items-center justify-center w-10 h-10 rounded-full bg-white shadow md:order-1">
                         
                         {/* centerSVG HERE */}
-                            <svg className="fill-emerald-500" xmlns="http://www.w3.org/2000/svg" width="16" height="16">
-                                <path d="M8 0a8 8 0 1 0 8 8 8.009 8.009 0 0 0-8-8Zm0 12a4 4 0 1 1 0-8 4 4 0 0 1 0 8Z" />
-                            </svg>
+                            {centerSVG}
                             {/* centerSVG HERE */}
 
                         </div>
                         {/* <!-- Date --> */}
-                        <time className="font-caveat font-medium text-xl text-indigo-500 md:w-28">{cardDate}</time>
+                        <time className="font-caveat font-medium text-l text-indigo-800 md:w-28">{cardDate}</time>
                     </div>
                     {/* <!-- Title --> */}
-                    <div className="text-slate-500 ml-14"><span className="text-slate-900 font-bold">{cardTitle}</span>
+                    <div className="text-slate-500 ml-14"><button className="text-slate-900 font-bold"><a href={titleLink}>{cardTitle}</a></button>
                         <div className="flex items-center">
                             <h1>{cardTitle2}</h1>
                             {titleSVG} {/* Icon for company */}
